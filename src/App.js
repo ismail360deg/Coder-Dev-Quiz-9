34781f5e8397a5e99cd0faee0a5314526ea22193
ismail360deg <ismailbhuiyan2000@gmail.com>
2022-10-12 16:00:00 +0600

    Toast added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom';
 import router from './utils/routes';
-
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
--- src/components/QuizDetails.jsx
+++ src/components/QuizDetails.jsx
@@ -1,25 +1,29 @@
 import { EyeIcon } from '@heroicons/react/24/solid';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const QuizDetails = () => {
     const quizTopic = useLoaderData();
     const data = quizTopic?.data?.questions;
 
     const handleAnsIcon = (ans) => {
-        alert(ans)
+        // alert(ans)
+        toast.success(ans, 'Correct Answer!', { autoClose: 500 })
     }
 
     const handleQuizAns = (e, correctAnswer) => {
         console.log(e, correctAnswer);
         if (e === correctAnswer) {
-            alert('right answer')
+            toast.success('Correct Answer!', { autoClose: 500 })
         }
         else {
-            alert('wrong answer')
+            toast.warning('Wrong Answer!', { autoClose: 500 })
         }
     }
     return (
-        <>
+        <div>
             {
                 data.map(d => <div key={d.id} className='bg-slate-400 lg:w-1/2 md:w-4/5 sm:w-11/12 w-full mb-10 p-10 rounded-xl mx-auto relative '>
                     <EyeIcon onClick={() => handleAnsIcon(d.correctAnswer)} className='absolute top-10 right-10 w-7 h-7 text-white'></EyeIcon>
@@ -42,9 +46,26 @@ const QuizDetails = () => {
                     </div>
                 </div>)
             };
-        </>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+        </div>
     )
 
 };
 
 export default QuizDetails;
+
+
+
+
+
